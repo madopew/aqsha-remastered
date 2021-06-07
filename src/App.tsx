7fde8b2db0606515fe03fc1c7397ead5e3e87006
Bakyt Madi <bakyt.madi.work@gmail.com>
2021-06-07 20:59:23 +0300

    Add dispatch logic

--- src/App.tsx
+++ src/App.tsx
@@ -7,23 +7,59 @@ import Balance from "./components/Balance/Balance";
 import Numpad from "./components/Numpad/Numpad";
 import useKeeper from "./hooks/KeeperHook";
 
+enum NumpadActionType {
+    update,
+    add,
+    withdraw,
+    unknown,
+    none,
+}
+
 function App() {
     const MAX_HISTORY = 10;
     const [total, updateTotal, today, add, withdraw, history, undo] =
         useKeeper(MAX_HISTORY);
 
     const [numpadVisible, setNumpadVisible] = useState(false);
+    const [numpadAction, setNumpadAction] = useState(NumpadActionType.none);
 
     const onAdd = () => {
+        // TODO: change to unknown
+        setNumpadAction(NumpadActionType.add);
         setNumpadVisible(true);
     };
 
     const onWithdraw = () => {
-        withdraw(10);
+        setNumpadAction(NumpadActionType.withdraw);
+        setNumpadVisible(true);
+    };
+
+    const dispatchNumpadAction = (val: number) => {
+        switch (numpadAction) {
+            case NumpadActionType.update:
+                updateTotal(val);
+                break;
+            case NumpadActionType.add:
+                add(val);
+                break;
+            case NumpadActionType.withdraw:
+                withdraw(val);
+                // TODO: show error message
+                break;
+            case NumpadActionType.none:
+                break;
+            default:
+                throw Error("Something wrong with numpad action!");
+        }
     };
 
     const onNumpadSuccess = (val: number) => {
-        console.log(val);
+        setNumpadVisible(false);
+        if (numpadAction === NumpadActionType.unknown) {
+            // TODO: ask for action type
+        }
+
+        dispatchNumpadAction(val);
     };
 
     return (
